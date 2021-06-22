f84612e0184aa2afab1940063731158319be3f84
Andrew <atg27@uw.edu>
2021-06-22 06:55:55 -0700

    commit tree 54149479f292c28716011b98e47fcfab93206638

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -2,11 +2,46 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
+    req = Rack::Request.new(env) 
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+    
+    elsif req.path.match(/publications/) 
+      if req.env["REQUEST_METHOD"] == "POST"
+        input = JSON.parse(req.body.read)
+        author_id = req.path.split('/authors/').last.split('/publications').last
+        author = Author.find_by(id: author_id)
+        publication = author.publications.create(title: input["title"])
+        return [200, { 'Content-Type' => 'application/json' }, [ publication.to_json ]]
+      elsif req.env["REQUEST_METHOD"] == "DELETE"
+        author_id = req.path.split('/authors/').last.split('/publications/').first
+        author = Author.find_by(id: author_id)
+        publication_id = req.path.split('/publications/').last
+        auth_publication = author.publications.find_by(id: publication_id)
+        auth_publication.destroy()
+      elsif req.env["REQUEST_METHOD"] == "PATCH"
+        input = JSON.parse(req.body.read)
+        author_id = req.path.split('/authors/').last.split('/publications/').first
+        author = Author.find_by(id: author_id)
+        publication_id = req.path.split('/publications/').last
+        auth_publication = author.publications.find_by(id: publication_id)
+        auth_publication.update(input)
+        return [200, { 'Content-Type' => 'application/json' }, [ auth_publication.to_json ]]
+      end
+    elsif req.path.match(/authors/) 
+        if req.env["REQUEST_METHOD"] == "POST"
+          input = JSON.parse(req.body.read)
+          author = Author.create(name: input["name"])
+          return [200, { 'Content-Type' => 'application/json' }, [ author.to_json ]]
 
+        else
+          if req.path.split("/authors").length == 0
+            return [200, { 'Content-Type' => 'application/json' }, [ Author.all.to_json ]]
+          else 
+            author_id = req.path.split("/authors/").last
+            return [200, { 'Content-Type' => 'application/json' }, [ Author.find_by(id: author_id).to_json({:include => :publications}) ]]
+          end
+        end
     else
       resp.write "Path Not Found"
 
